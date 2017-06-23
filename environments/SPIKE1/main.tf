ca37cf9653e8ee619e4f3d261b0630c12655ae63
keith <keithgair@yahoo.co.uk>
2017-06-23 16:20:42 +0100

    using GitHub for the modules source URLs

--- environments/SPIKE1/main.tf
+++ environments/SPIKE1/main.tf
@@ -13,19 +13,22 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "github.com/cageykg/spike1-pipeline//modules/vpc"
+  #source    = "../../modules/vpc"
+  source     = "github.com/cageykg/spike1-pipeline//modules/vpc?ref=1.0.0"
+
   vpc_name   = "TABI-SPIKE1"
   cidr_block = "10.105.0.0/16"
 }
 
 module "microservice" {
-    source = "github.com/cageykg/spike1-pipeline//modules/microservice"
+  #source = "../../modules/microservice"
+  source = "github.com/cageykg/spike1-pipeline//modules/microservice?ref=1.0.0"
 
-    service_name = "service-one"
-    service_port = 8081
-    service_image_id = "ami-4d3a2e29"
-    vpc_name = "${module.vpc.aws_vpc.vpc.name}"
-    vpc_id = "${module.vpc.aws_vpc.vpc.id}"
-    internal_subnet_id = "${module.vpc.aws_subnet.internal_subnet.*.id[0]}"
-    external_subnet_id = "${module.vpc.aws_subnet.external_subnet.*.id[0]}"
+  service_name = "service-one"
+  service_port = 8081
+  service_image_id = "ami-4d3a2e29"
+  vpc_name = "${module.vpc.aws_vpc.vpc.name}"
+  vpc_id = "${module.vpc.aws_vpc.vpc.id}"
+  internal_subnet_id = "${module.vpc.aws_subnet.internal_subnet.*.id[0]}"
+  external_subnet_id = "${module.vpc.aws_subnet.external_subnet.*.id[0]}"
 }
